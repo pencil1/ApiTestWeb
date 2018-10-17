7faf13598982926ea7a195a4608404185288f178
fke <362508572@qq.com>
2018-10-17 15:29:27 +0800

    优化代码

--- src/projectManage/case/case.vue
+++ src/projectManage/case/case.vue
@@ -10,18 +10,21 @@
                     </el-option>
                 </el-select>
 
-                <el-select v-model="form.modelName" placeholder="请选择模块" style="width: 150px">
+                <el-select v-model="form.module" placeholder="请选择模块" value-key="moduleId" style="width: 150px">
                     <el-option
                             v-for="item in proModelData[this.form.projectName]"
-                            :key="item"
+                            :key="item.moduleId"
+                            :label="item.name"
                             :value="item">
                     </el-option>
                 </el-select>
 
-                <el-select v-model="form.configName" placeholder="请选择配置" style="width: 150px">
+                <el-select v-model="form.config" placeholder="请选择配置" clearable value-key="configId" style="width: 150px">
                     <el-option
+
                             v-for="item in configNameData[this.form.projectName]"
-                            :key="item"
+                            :key="item.configId"
+                            :label="item.name"
                             :value="item">
                     </el-option>
                 </el-select>
@@ -39,7 +42,6 @@
             <el-form-item>
                 <el-button type="primary" icon="el-icon-search" @click.native="findDataBtn()">搜索</el-button>
                 <el-button type="primary" @click.native="initData()">录入接口信息</el-button>
-                <el-button type="primary" @click.native="initData()">录入接口信息</el-button>
                 <el-button type="primary"
                            v-if="showNumTab === 'first'"
                            @click.native="apiTest(casesList)">测试
@@ -48,7 +50,7 @@
                            @click.native="apiTest(null, suiteList)">测试
                 </el-button>
                 <el-button type="primary" icon="el-icon-view" @click.native="$refs.resultFunc.lastResult()"></el-button>
-                <el-button type="primary" @click.native="initSuiteView()">添加套件</el-button>
+                <!--<el-button type="primary" @click.native="initSuiteView()">添加套件</el-button>-->
                 <el-button type="primary" @click.native="$refs.importApiFunc.initData()">导入信息</el-button>
             </el-form-item>
         </el-form>
@@ -112,64 +114,64 @@
                     </div>
                 </el-tab-pane>
 
-                <el-tab-pane label="接口套件" name="third">
-                    <el-table
-                            ref="suiteMultipleTable"
-                            @selection-change="handleSuiteSelection"
-                            :data="suiteTableData"
-                            stripe>
-                        <el-table-column
-                                type="selection"
-                                width="55">
-                        </el-table-column>
-                        <el-table-column
-                                prop="num"
-                                label="编号"
-                                width="60">
-                        </el-table-column>
-                        <el-table-column
-                                :show-overflow-tooltip=true
-                                prop="name"
-                                label="套件名称"
-                                width="200">
-                        </el-table-column>
-                        <el-table-column
-                                :show-overflow-tooltip=true
-                                prop="api_names"
-                                label="接口信息">
-                        </el-table-column>
-                        <el-table-column
-                                label="操作"
-                                width="320">
-                            <template slot-scope="scope">
-                                <el-button type="primary" icon="el-icon-edit" size="mini"
-                                           @click.native="editSuite(suiteTableData[scope.$index]['id'])">编辑
-                                </el-button>
-                                <el-button type="danger" icon="el-icon-delete" size="mini"
-                                           @click.native="sureView(delSuite,suiteTableData[scope.$index]['id'])">删除
-                                </el-button>
-                            </template>
-                        </el-table-column>
-                    </el-table>
+                <!--<el-tab-pane label="接口套件" name="third">-->
+                    <!--<el-table-->
+                            <!--ref="suiteMultipleTable"-->
+                            <!--@selection-change="handleSuiteSelection"-->
+                            <!--:data="suiteTableData"-->
+                            <!--stripe>-->
+                        <!--<el-table-column-->
+                                <!--type="selection"-->
+                                <!--width="55">-->
+                        <!--</el-table-column>-->
+                        <!--<el-table-column-->
+                                <!--prop="num"-->
+                                <!--label="编号"-->
+                                <!--width="60">-->
+                        <!--</el-table-column>-->
+                        <!--<el-table-column-->
+                                <!--:show-overflow-tooltip=true-->
+                                <!--prop="name"-->
+                                <!--label="套件名称"-->
+                                <!--width="200">-->
+                        <!--</el-table-column>-->
+                        <!--<el-table-column-->
+                                <!--:show-overflow-tooltip=true-->
+                                <!--prop="api_names"-->
+                                <!--label="接口信息">-->
+                        <!--</el-table-column>-->
+                        <!--<el-table-column-->
+                                <!--label="操作"-->
+                                <!--width="320">-->
+                            <!--<template slot-scope="scope">-->
+                                <!--<el-button type="primary" icon="el-icon-edit" size="mini"-->
+                                           <!--@click.native="editSuite(suiteTableData[scope.$index]['id'])">编辑-->
+                                <!--</el-button>-->
+                                <!--<el-button type="danger" icon="el-icon-delete" size="mini"-->
+                                           <!--@click.native="sureView(delSuite,suiteTableData[scope.$index]['id'])">删除-->
+                                <!--</el-button>-->
+                            <!--</template>-->
+                        <!--</el-table-column>-->
+                    <!--</el-table>-->
 
-                    <el-button @click="cancelSelection()" size="mini" style="position: absolute;margin-top: 2px;">取消选择
-                    </el-button>
-                    <div class="pagination">
-                        <el-pagination
-                                @current-change="handleCurrentChange"
-                                @size-change="handleSizeChange"
-                                :page-size="20"
-                                layout="total, sizes, prev, pager, next, jumper"
-                                :total="this.suitePage.total">
-                        </el-pagination>
-                    </div>
-                </el-tab-pane>
+                    <!--<el-button @click="cancelSelection()" size="mini" style="position: absolute;margin-top: 2px;">取消选择-->
+                    <!--</el-button>-->
+                    <!--<div class="pagination">-->
+                        <!--<el-pagination-->
+                                <!--@current-change="handleCurrentChange"-->
+                                <!--@size-change="handleSizeChange"-->
+                                <!--:page-size="20"-->
+                                <!--layout="total, sizes, prev, pager, next, jumper"-->
+                                <!--:total="this.suitePage.total">-->
+                        <!--</el-pagination>-->
+                    <!--</div>-->
+                <!--</el-tab-pane>-->
                 <el-tab-pane label="接口配置" name="second" v-if="apiEditViewStatus"
                              style="background-color: rgb(250, 250, 250);min-height: 780px">
                     <apiEdit
                             :projectName="form.projectName"
-                            :modelName="form.modelName"
-                            :configName="form.configName"
+                            :module="form.module"
+                            :configId="form.config.configId"
                             :proModelData="proModelData"
                             :proUrlData="proUrlData"
                             @findApi="findCases"
@@ -182,12 +184,12 @@
 
         </el-scrollbar>
 
-        <suiteEdit
-                :projectName="form.projectName"
-                :modelName="form.modelName"
-                :casesList="casesList"
-                ref="suiteFunc">
-        </suiteEdit>
+        <!--<suiteEdit-->
+                <!--:projectName="form.projectName"-->
+                <!--:model="form.model"-->
+                <!--:casesList="casesList"-->
+                <!--ref="suiteFunc">-->
+        <!--</suiteEdit>-->
 
         <importApi
                 :projectName="form.projectName"
@@ -246,11 +248,19 @@
                     sizePage: 20,
                 },
                 form: {
+                    config: {
+                        name: null,
+                        configId: null,
+                    },
+                    module: {
+                        name: null,
+                        moduleId: null,
+                    },
                     projectName: null,
-                    configName: null,
+                    projectId: null,
                     suiteName: null,
                     apiName: null,
-                    modelName: null,
+
                 },
             }
         },
@@ -262,10 +272,14 @@
                         this.configNameData = response.data['config_name_list'];
                         this.proUrlData = response.data['urlData'];
                         this.form.projectName = response.data['user_pro']['pro_name'];
-                        this.form.configName = response.data['config_name_list'][this.form.projectName][0].toString();
-                        this.form.modelName = response.data['user_pro']['model_list'][0].toString();
+                        // this.form.config.configName = response.data['config_name_list'][this.form.projectName][0]['name'].toString();
+                        // this.form.config.configId = response.data['config_name_list'][this.form.projectName][0]['configId'].toString();
+                        // this.form.modelName = response.data['user_pro']['model_list'][0]['name'].toString();
+                        // this.form.modelId = response.data['user_pro']['model_list'][0]['moduleId'].toString();
+                        this.form.module = response.data['user_pro']['model_list'][0];
+                        this.form.config = this.configNameData[this.form.projectName][0];
                         this.findCases();
-                        this.findSuite();
+                        // this.findSuite();
                     }
                 )
             },
@@ -315,10 +329,11 @@
                 )
             },
             findCases() {
+
                 this.$axios.post('/api/api/cases/find', {
                     'caseName': this.form.apiName,
                     'projectName': this.form.projectName,
-                    'gatName': this.form.modelName,
+                    'moduleId': this.form.module.moduleId,
                     'page': this.apiPage.currentPage,
                     'sizePage': this.apiPage.sizePage,
                 }).then((response) => {
@@ -367,14 +382,13 @@
                     }
                 )
             },
-            apiTest(caseData=null,suiteData=null) {
+            apiTest(caseData = null, suiteData = null) {
                 this.loading = true;
                 this.$axios.post('/api/api/cases/run', {
                     'caseData': caseData,
                     'suiteData': suiteData,
                     'projectName': this.form.projectName,
-                    'gathers': this.form.modelName,
-                    'configName': this.form.configName,
+                    'configId': this.form.config.configId,
                 }).then((response) => {
                         if (response.data['status'] === 0) {
                             this.$message({
@@ -424,8 +438,8 @@
                 this.$refs.suiteMultipleTable.clearSelection();
             },
             clearChoice() {
-                this.form.modelName = null;
-                this.form.configName = null;
+                this.form.config = null;
+                this.form.module = null;
             },
 
         },
@@ -443,10 +457,6 @@
 
     .CodeMirror-gutter {
         width: 40px;
-    }
-
-    .el-message-box {
-        width: 800px;
     }
 
     .cm-s-default .cm-string {
